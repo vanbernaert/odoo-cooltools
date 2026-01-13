addc8e4fc9b64569b1d33d89ade484830b2a2ceb
Kristof Bernaert <kristof@vanbernaert.eu>
2026-01-13 18:29:49 +0100

    initial upload

---
+++ partner_hierarchical_name/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Partner Hierarchical Name",
+    "version": "16.0.1.0.0",
+    "summary": "Show full partner hierarchy in dropdowns (Company / Delivery / Contact)",
+    "category": "Contacts",
+    "author": "Your Company",
+    "license": "LGPL-3",
+    "depends": ["base"],
+    "installable": True,
+    "application": False,
+}
