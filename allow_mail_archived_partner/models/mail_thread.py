224ebb3713cdb8f0ceb615562b802e565fb525af
Kristof Bernaert <kristof@vanbernaert.eu>
2026-01-15 11:49:31 +0100

    fix

--- allow_mail_archived_partner/models/mail_thread.py
+++ allow_mail_archived_partner/models/mail_thread.py
@@ -3,14 +3,20 @@ from odoo import models, api
 class MailThread(models.AbstractModel):
     _inherit = "mail.thread"
 
+    def _notify_thread(self, message, msg_vals=False, **kwargs):
+        """Override to add context before calling parent method"""
+        if self._name in ("sale.order", "account.move"):
+            # Add context flag for partner searches
+            self = self.with_context(mail_notification=True, active_test=False)
+        
+        return super()._notify_thread(message, msg_vals=msg_vals, **kwargs)
+
     def _notify_get_recipients(self, message, msg_vals):
         """Force include archived partners for sales/invoices"""
-        if self._name in ("sale.order", "account.move"):
-            # Add context flag before calling parent
-            self = self.with_context(mail_notification=True)
-        
+        # First get recipients normally
         recipients = super()._notify_get_recipients(message, msg_vals)
         
+        # Only process for sales orders and invoices
         if self._name in ("sale.order", "account.move"):
             # Re-fetch any partner recipients without active filter
             for recipient in recipients:
@@ -18,6 +24,7 @@ class MailThread(models.AbstractModel):
                     partner = self.env['res.partner'].with_context(
                         active_test=False
                     ).browse(recipient['partner_id'])
+                    
                     if partner.exists():
                         recipient.update({
                             'partner': partner,
