659b5890a7583171924ac577e21c73cc72e8af09
Kristof Bernaert <kristof@vanbernaert.eu>
2026-01-26 01:01:44 +0100

    Fix allow_mail_archived_partner causing system notifications to send emails

--- allow_mail_archived_partner/models/__init__.py
+++ allow_mail_archived_partner/models/__init__.py
@@ -2,5 +2,5 @@
 from . import mail_thread
 from . import res_partner
 from . import mail_template
-# from . import mail_compose_message
-from . import account_move_send
+from . import mail_compose_message
+#from . import account_move_send
