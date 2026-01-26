38f3ca8774cd699d2241c007e230ddcaa036359c
Kristof Bernaert <kristof@vanbernaert.eu>
2026-01-26 02:12:39 +0100

    Fix allow_mail_archived_partner causing system notifications to send emails

--- allow_mail_archived_partner/models/account_move_send.py
+++ allow_mail_archived_partner/models/account_move_send.py
@@ -1,30 +1,37 @@
 import logging
 _logger = logging.getLogger(__name__)
 
-from odoo import models, api
+from odoo import models, api, fields
 
 
 class AccountInvoiceSend(models.TransientModel):
     _inherit = "account.invoice.send"
 
+    # OVERRIDE THE FIELD DEFINITION to remove domain filter
+    partner_ids = fields.Many2many(
+        'res.partner',
+        string='Recipients',
+        help='Contacts of the invoice that will receive the email.',
+        # REMOVE any domain that filters by active=True
+        context={'active_test': False},  # Allow archived in searches
+        check_company=True,
+    )
+
     @api.model
     def default_get(self, fields):
         """
-        Force inclusion of archived partners for invoice email wizard
+        Pre-fill invoice email wizard with archived partners.
         """
-        _logger.info("=== ACCOUNT.INVOICE.SEND DEFAULT_GET ===")
-        _logger.info(f"Fields: {fields}")
-        _logger.info(f"Context: {self.env.context}")
+        _logger.debug("=== ACCOUNT.INVOICE.SEND DEFAULT_GET ===")
         
-        # Get the default result first
         res = super().default_get(fields)
-        _logger.info(f"Super result partner_ids: {res.get('partner_ids')}")
+        _logger.debug(f"Super result partner_ids: {res.get('partner_ids')}")
         
         active_ids = self.env.context.get('active_ids', [])
-        _logger.info(f"Active IDs: {active_ids}")
+        _logger.debug(f"Active IDs: {active_ids}")
         
         if active_ids:
-            # Find invoices WITH archived partners allowed
+            # Find invoices with archived partners allowed
             moves = self.env['account.move'].with_context(
                 active_test=False
             ).browse(active_ids)
@@ -33,28 +40,24 @@ class AccountInvoiceSend(models.TransientModel):
             for move in moves:
                 if move.partner_id:
                     partner_ids.append(move.partner_id.id)
-                    _logger.info(f"Found partner {move.partner_id.id} (active={move.partner_id.active}) for invoice {move.id}")
+                    _logger.debug(f"Found partner {move.partner_id.id} for invoice {move.id}")
             
             if partner_ids:
                 res['partner_ids'] = [(6, 0, partner_ids)]
-                _logger.info(f"SET partner_ids: {res['partner_ids']}")
-            else:
-                _logger.info("No partners found for invoices")
+                _logger.debug(f"SET partner_ids: {res['partner_ids']}")
         
         return res
 
     def _get_composer_values(self, res_ids, template):
         """
-        Pass context to allow archived partners in email composition
+        Pass context to mail composer to allow archived partners.
         """
-        _logger.info("=== ACCOUNT.INVOICE.SEND _get_composer_values ===")
-        _logger.info(f"Passing context to allow archived partners")
-        
+        _logger.debug("Passing context to allow archived partners")
         return super(
             AccountInvoiceSend,
             self.with_context(
                 active_test=False,
-                include_archived_partners=True,
+                include_archived_partner=True,
                 mail_notify_force=True,
             )
         )._get_composer_values(res_ids, template)
